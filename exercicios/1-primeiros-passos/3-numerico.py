4412eab42ec6d0b98f97e1edd46e258eed9ebf18
almeidacra <161429326+almeidacra@users.noreply.github.com>
2024-03-07 06:05:13 +0000

    resolucao

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,12 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print (type(idade_numerica))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
-
+print('Paloma'*2)
 # Realize uma operação entre dados do tipo inteiro e float
+soma = idade + 2.5
+print (type(soma))
+print (soma)
+print(2+3.4)
